b97f2805ed7fe78f787d0b287a9f009520a5ca66
markmiet <minamake@hotmail.com>
2018-10-10 23:45:46 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("markontesti");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
