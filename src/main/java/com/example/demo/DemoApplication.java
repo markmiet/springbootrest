ea1f1ad66d984d5e1e72dd0dac6708937d79433d
markmiet <minamake@hotmail.com>
2018-10-12 19:27:32 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("maa11markontesti22 ds");
+		System.out.println("maa11markontesti22 ds");
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
