37dcd83c0e061f3605a97b63b41fc8b1fd37e903
markmiet <minamake@hotmail.com>
2018-10-12 21:56:39 +0300

    muutos

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("testimaa11markontesti22 ds");
+		System.out.println("muutostestimaa11markontesti22 ds");
 		System.out.println("maa11markontesti22 ds");
 		
 		SpringApplication.run(DemoApplication.class, args);
